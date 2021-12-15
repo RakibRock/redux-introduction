b1639e90bdcb631f57e38917a62122228b759716
RakibRahman <rakibrahman934@gmail.com>
2021-12-15 12:00:53 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
 function App() {
-  const account = useSelector((state) => state);
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
 
-  return <div className="App"></div>;
+  const AC = bindActionCreators(actionCreators, dispatch);
+  console.log(AC);
+
+  return (
+    <div className="App">
+      <h1>{account}</h1>
+      <button>Deposit</button>
+      <button>Withdraw</button>
+    </div>
+  );
 }
 
 export default App;
